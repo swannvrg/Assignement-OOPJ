a37466572a25ee85a7f1446c7e75cb76ce087c6c
snn <131522064+swannvrg@users.noreply.github.com>
2025-12-11 18:07:29 +0800

    chang

--- src/assignement_oopj/EmailService.java
+++ src/assignement_oopj/EmailService.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package assignment_oopj;
+package assignement_oopj;
 
 public class EmailService {
     private String userEmail = "marcelariandy@gmail.com"; // Your email
